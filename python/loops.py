6b379531dfaed746e04ee40bcc214629bd502732
KristianColville1 <kristiancolville1@gmail.com>
2022-02-01 10:50:24 +0000

    Add python for loop with correct data type for iteration

--- python/loops.py
+++ python/loops.py
@@ -1,10 +1,10 @@
-a = 23
-d = 342
-j = 2
+a = '23'
+d = '342'
+j = '2'
 
 # above are 3 basic integer variables.
 
 # this is a for loop
 
-for n in d:
-    print(n + a - j)
+for c in d:
+    print(c + a + j)
