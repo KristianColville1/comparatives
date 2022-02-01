b4730754e6f7caef522177bdf1fe0fad2f28fa53
KristianColville1 <kristiancolville1@gmail.com>
2022-02-01 11:01:33 +0000

    Add while loop to practice interating with this type of loop in python

--- python/loops.py
+++ python/loops.py
@@ -8,3 +8,32 @@ j = '2'
 
 for c in d:
     print(c + a + j)
+
+
+# this is a bad while loop, this will break your computer. It never ends
+
+# while(a < d):
+#     a += j
+#     print(j)
+
+# below is a while loop implemented correctly
+
+apples = 10
+oranges = 32423
+tomatoes = 34
+
+# while(apples < oranges):
+#     apples += tomatoes
+#     print('Picking apples all day.. when will it end..')
+
+# I decided to build a counter for the while loop to see how many iterations it took to complete its task.
+
+iterations = 0
+while(apples < oranges):
+    apples += tomatoes
+
+    iterations += 1
+
+print(iterations)
+
+# It took 954 iterations to complete my while loop, abit much for an example so its commented out.
