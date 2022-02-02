c5babd88b1220bbc0c32fed984aa2ba0d78434aa
KristianColville1 <kristiancolville1@gmail.com>
2022-02-02 08:41:30 +0000

    Add data structure examples in python

---
+++ python/data-structures.py
@@ -0,0 +1,46 @@
+# example of an array/list
+
+school = [
+    'CBS', '21 Bridget terrace', 'Math', 'English', 'Irish', 'Physics', 'Construction studies', 'Art History', 'Geography'
+]
+
+# Nested array/list. Two dimensional.
+
+holidays = [
+    ['Christmas', 'Roast dinner', 'Family', 'Warmth'],
+    ['Thanksgiving', 'History', 'Family', 'Friendship'],
+    ['Valentines', 'Love', 'Warmth']
+]
+
+# example of an object/dictionary
+
+life = {
+    'thomas': 'sad',
+    'mary': 'bad',
+    'candice': 'sweet',
+    'billy': 'neat',
+    'joe': 'smelly',
+    'grandad': 'telly'
+}
+
+# Nested object/dictionary. two dimensional.
+
+personality = {
+    'aggressive': {
+        'mood': 'hostile',
+        'violence_rate': 'high',
+        'fuse': 'short'
+    },
+
+    'introvert': {
+        'mood': 'calm',
+        'violence_rate': 'low',
+        'fuse': 'long'
+    },
+
+    'extratrovert': {
+        'mood': 'bouncy',
+        'violence_rate': 'medium',
+        'fuse': 'medium'
+    }
+}
