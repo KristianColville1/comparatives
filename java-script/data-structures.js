2c5f0e056086c89e2b9e74fd2032857ff0154824
KristianColville1 <kristiancolville1@gmail.com>
2022-02-02 08:26:23 +0000

    Add arrays and content to arrays

---
+++ java-script/data-structures.js
@@ -0,0 +1,15 @@
+// example of an array/list
+
+let school = [
+    'CBS', '21 Bridget terrace', 'Math', 'English', 'Irish', 'Physics', 'Construction studies', 'Art History', 'Geography'
+]
+
+// Nested array/list. Two dimensional.
+
+let holidays = [
+    ['Christmas', 'Roast dinner', 'Family', 'Warmth'],
+    ['Thanksgiving', 'History', 'Family', 'Friendship'],
+    ['Valentines', 'Love', 'Warmth']
+]
+
+// 
